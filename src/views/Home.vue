091fa0f16539e1679b36be85e36344a44edf0afd
Kelly Zick <kllyzck@gmail.com>
2019-07-27 21:48:37 -0600

    Add t-rex image and clean up demo text

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img
+      alt="A very fancy T-rex holds a large book while wearing her pearls and best hat"
+      class="dino-img"
+      src="../assets/tyrannothesaurus.png"
+    />
+    <HelloWorld msg="Tyrannothesaurus" />
+    <Synonyms />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Synonyms from '@/components/Synonyms.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    Synonyms
+  }
+}
+</script>
+
+<style>
+.dino-img {
+  width: 30vh;
+}
+</style>
