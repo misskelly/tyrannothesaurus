c7fa3d07e13b7e1d3792ed340144e9683b369148
Kelly Zick <kllyzck@gmail.com>
2019-07-28 12:09:45 -0600

    Hook up api call and dom render synonyms

--- src/components/Synonyms.vue
+++ src/components/Synonyms.vue
@@ -1,55 +1,72 @@
 <template>
-  <section>
-
-    <input v-
-    <h2>{{ heading }}</h2>
-    <ul v-if="words && words.length">
-      <a class="word-link" href="#">
-        <li v-for="word of words" :key="word">{{ word }}</li>
-      </a>
-    </ul>
-    <ul v-if="errors && errors.length">
-      <li v-for="error of errors">{{ error.message }}</li>
-    </ul>
-  </section>
+  <v-container>
+    <v-content>
+      <v-text-field
+        v-model="queryWord"
+        label="Enter a word"
+        solo
+        @keydown.enter="lookUp"
+      >
+        <v-fade-transition slot="append">
+          <v-icon v-if="queryWord" @click="lookUp"
+            >mdi-book-search-outline</v-icon
+          >
+        </v-fade-transition>
+      </v-text-field>
+      <h2>{{ heading }}</h2>
+      <v-card>
+        <ul v-if="words && words.length">
+          <a class="word-link" href="#">
+            <li v-for="word of words" :key="word">{{ word }}</li>
+          </a>
+        </ul>
+        <ul v-if="errors && errors.length">
+          <li
+            v-for="error of errors"
+            :key="`${this.queryWord}_${error.message}`"
+          >
+            {{ error.message }}
+          </li>
+        </ul>
+      </v-card>
+    </v-content>
+  </v-container>
 </template>
 
 <script>
-import axios from 'axios'
-import { key } from '@/assets/key.js'
+import getWords from '../utils/apiCall'
+
 export default {
   name: 'Synonyms',
-  props: {
-    queryWord: String
-  },
   data() {
     return {
-      searched: this.queryWord,
+      queryWord: null,
       heading: '',
       words: [],
       errors: []
     }
   },
+  methods: {
+    lookUp() {
+      console.log('got to lookUp')
+      getWords(this.queryWord)
+        .then(result => {
+          if (result.synonyms) {
+            this.words = result.synonyms
+            this.heading = `Synonyms for ${queryWord}`
+          } else if (result.didYouMean) {
+            this.heading =
+              'Hmm...Tyrannothesaurus could not find that word.  Did you mean one of these?'
+            this.words = result.didYouMean
+          }
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
+    }
+  },
   created() {
-    axios
-      .get(
-        `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${searched}?key=${key}`
-      )
-      .then(response => {
-        console.log(response)
-        if (response.data[0].meta) {
-          this.words = response.data[0].meta.syns[0]
-          this.heading = `Synonyms for ${searched}`
-        } else {
-          this.heading =
-            'Hmm...Tyrannothesaurus could not find that word.  Did you mean one of these?'
-          this.words = response.data
-        }
-        console.log('words', this.words)
-      })
-      .catch(e => {
-        this.errors.push(e)
-      })
+    getWords('great').then(result => console.log(result))
   }
 }
 </script>
@@ -59,6 +76,4 @@ ul {
   list-style-type: none;
   padding: 0;
 }
-/* .syn-link {
-} */
 </style>
