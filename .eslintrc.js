a1fe8ba99d6d8f0cdafa95d2903629a58f5765a2
Kelly Zick <kllyzck@gmail.com>
2019-07-28 15:39:22 -0600

    Update linter config and lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
-    module.exports = {
-      root: true,
-      env: {
-        node: true
-      },
-      'extends': [
-        'plugin:vue/essential',
-        'plugin:prettier/recommended',
-        '@vue/prettier'
-      ],
-      rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-      },
-      parserOptions: {
-        parser: 'babel-eslint'
-      }
-    }
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended',
+    '@vue/prettier'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/array-bracket-spacing': 'warning'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
