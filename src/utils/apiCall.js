175c9b9dbb829409e3e8890591d229263d5dcac0
Kelly Zick <kllyzck@gmail.com>
2019-07-30 20:28:49 -0600

    Add environment variable

--- src/utils/apiCall.js
+++ src/utils/apiCall.js
@@ -1,28 +1,26 @@
-import { key } from '@/assets/key.js'
 import axios from 'axios'
 
-const getWords = query => {
-  return axios
-    .get(
+const getWords = async query => {
+  const key = process.env.VUE_APP_API_KEY
+  try {
+    const response = await axios.get(
       `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${query}?key=${key}`
     )
-    .then(response => {
-      console.log(response)
-      if (response.data[0].meta) {
-        return {
-          synonyms: response.data[0].meta.syns[0]
-        }
-      } else {
-        return {
-          didYouMean: response.data
-        }
-      }
-    })
-    .catch(e => {
+    console.log(response)
+    if (response.data[0].meta) {
       return {
-        error: e.message
+        synonyms: response.data[0].meta.syns[0]
       }
-    })
+    } else {
+      return {
+        didYouMean: response.data
+      }
+    }
+  } catch (e) {
+    return {
+      error: e.message
+    }
+  }
 }
 
 export default getWords
