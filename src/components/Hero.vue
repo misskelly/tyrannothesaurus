7d6fbdd78c66a62378728cd93f7dc1cab76aa64d
Kelly Zick <kllyzck@gmail.com>
2019-07-30 20:30:38 -0600

    Adjust styling

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,15 +3,15 @@
     <v-layout text-center wrap>
       <v-flex xs12>
         <h1 class="main-heading">Tyrannothesaurus</h1>
-        <h2 class="text-bubble">
+        <!-- <h2 class="text-bubble">
           Salutations, perchance might I engage you with an assemblage of
           sophisticated verbalism?
-        </h2>
+        </h2> -->
         <v-img
           :src="require('../assets/tyrannothesaurus.png')"
           class="my-3"
           contain
-          height="300"
+          height="425"
         ></v-img>
       </v-flex>
     </v-layout>
@@ -26,21 +26,8 @@ export default {
 
 <style>
 .main-heading {
-  color: lime darken-4;
-  font-family: 'IM Fell Great Primer SC', serif;
-  font-size: 3rem;
-  margin: 3rem auto;
-}
-.text-bubble {
-  width: 40%;
-  position: absolute;
-  right: 12%;
-  top: 9rem;
-  z-index: 3;
-  font-size: 1rem;
-  text-align: left;
-  background: hsla(0, 0%, 90%, 0.488);
-  padding: 0.5rem;
-  border-radius: 10px;
+  font-family: 'Miss Fajardose', cursive;
+  text-shadow: 3px 3px 3px hsla(54, 70%, 30%, 0.8);
+  font-size: 12vw;
 }
 </style>
